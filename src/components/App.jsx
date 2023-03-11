18b8e7b4389082b293747aaee08641456fecbeac
OleksandrDika <109437683+OleksandrDika@users.noreply.github.com>
2023-03-11 12:00:50 +0200

    make

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,18 +1,39 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { nanoid } from 'nanoid';
 import { ContactForm } from './ContactForm/ContactForm';
 import { Filter } from './Filter/Filter';
 import { ContactList } from './ContactList/ContactList';
 import { Conteiner } from './Conteiner.styled';
+import startContacts from './contacts.json';
 
 export const App = () => {
-  const [contacts, setContacts] = useState([
-    { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-    { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-    { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-    { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-  ]);
+  const [contacts, setContacts] = useState([]);
   const [filter, setFilter] = useState('');
+
+  useEffect(() => {
+    const localStorCont = localStorage.getItem('contacts');
+    console.log(localStorCont);
+    if (localStorCont !== null) {
+      const parseCont = JSON.parse(localStorCont);
+      setContacts(parseCont);
+      return;
+    }
+    // console.log(startContacts);
+    setContacts(startContacts);
+  }, []);
+
+  useEffect(() => {
+    if (contacts.length === 0) {
+      return;
+    }
+    localStorage.setItem('contacts', JSON.stringify(contacts));
+  }, [contacts]);
+
+  // componentDidUpdate(prevProps, prevState) {
+  //   if (prevState.contacts !== this.state.contacts) {
+  //     localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+  //   }
+  // }
 
   const changeFilter = event => {
     setFilter(event.currentTarget.value);
